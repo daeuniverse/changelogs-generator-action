1968a9f5fd6db3934fad5fa853f1c5ffb6a7d4b6
Kevin YU <yqlbu@bu.edu>
2023-06-23 13:54:05 +0800

    ci:fix syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+APP_DIR=/app
+
 if [ -n "$INPUT_TOKEN" ]; then GITHUB_TOKEN="$INPUT_TOKEN"; fi
 if [ -n "$INPUT_FUTURERELEASE" ]; then FUTURE_TAG="$INPUT_FUTURERELEASE"; fi
 if [ -n "$INPUT_PREVIOUSRELEASE" ]; then PREVIOUS_TAG="$INPUT_PREVIOUSRELEASE"; fi
 
-node generate-changelogs.mjs
+node ${APP_DIR}/generate-changelogs.mjs
